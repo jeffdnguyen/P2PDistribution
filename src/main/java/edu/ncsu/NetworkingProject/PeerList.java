57a9fc2fdea4d369a2d0647d6d95e6f49e720382
Jeff Nguyen <jnguyen8@ncsu.edu>
2019-02-06 13:33:16 -0500

    Created PeerList data structure

---
+++ src/main/java/edu/ncsu/NetworkingProject/PeerList.java
@@ -0,0 +1,80 @@
+package edu.ncsu.NetworkingProject;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class PeerList implements Serializable {
+
+    private static final long serialVersionUID = -140841477817596923L;
+
+    private String            hostname;
+
+    private int               cookie;
+
+    private boolean           isActive;
+
+    private int               TTL;
+
+    private int               portNumber;
+
+    private int               numberOfTimesActive;
+
+    private Date              lastActive;
+
+    public String getHostname () {
+        return hostname;
+    }
+
+    public void setHostname ( final String hostname ) {
+        this.hostname = hostname;
+    }
+
+    public int getCookie () {
+        return cookie;
+    }
+
+    public void setCookie ( final int cookie ) {
+        this.cookie = cookie;
+    }
+
+    public boolean isActive () {
+        return isActive;
+    }
+
+    public void setActive ( final boolean isActive ) {
+        this.isActive = isActive;
+    }
+
+    public int getTTL () {
+        return TTL;
+    }
+
+    public void setTTL ( final int tTL ) {
+        TTL = tTL;
+    }
+
+    public int getPortNumber () {
+        return portNumber;
+    }
+
+    public void setPortNumber ( final int portNumber ) {
+        this.portNumber = portNumber;
+    }
+
+    public int getNumberOfTimesActive () {
+        return numberOfTimesActive;
+    }
+
+    public void setNumberOfTimesActive ( final int numberOfTimesActive ) {
+        this.numberOfTimesActive = numberOfTimesActive;
+    }
+
+    public Date getLastActive () {
+        return lastActive;
+    }
+
+    public void setLastActive ( final Date lastActive ) {
+        this.lastActive = lastActive;
+    }
+
+}
