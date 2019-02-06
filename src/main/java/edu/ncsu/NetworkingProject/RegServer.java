60612459c212ae6c254f427a1cc160dd4b29d79c
Jeff Nguyen <jnguyen8@ncsu.edu>
2019-02-06 13:19:08 -0500

    Added welcome and connection sockets to RegServer.java

--- src/main/java/edu/ncsu/NetworkingProject/RegServer.java
+++ src/main/java/edu/ncsu/NetworkingProject/RegServer.java
@@ -1,9 +1,20 @@
 package edu.ncsu.NetworkingProject;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class RegServer {
 
-    public static void main(String[] args) {
-        System.out.println("I'm a server!");
-    }
+    public static void main ( final String[] args ) {
+        ServerSocket welcomeSocket;
+        try {
+            welcomeSocket = new ServerSocket( 65423 );
+            final Socket connectionSocket = welcomeSocket.accept();
+        }
+        catch ( final IOException e ) {
+            e.printStackTrace();
 
+        }
+    }
 }
