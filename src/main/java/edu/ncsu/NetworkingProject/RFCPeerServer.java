f89a19c1a20af8dee087a73c826b6d86a22f44f3
Wynne Plaga <rwplaga@ncsu.edu>
2019-02-25 15:38:24 -0500

    Make peerServer parallel

--- src/main/java/edu/ncsu/NetworkingProject/RFCPeerServer.java
+++ src/main/java/edu/ncsu/NetworkingProject/RFCPeerServer.java
@@ -7,7 +7,10 @@ import edu.ncsu.NetworkingProject.protocol.Status;
 import edu.ncsu.NetworkingProject.protocol.messages.GetRFCMessage;
 import edu.ncsu.NetworkingProject.protocol.messages.RFCQueryMessage;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -50,46 +53,37 @@ public class RFCPeerServer implements Runnable {
 
     @Override
     public void run() {
-        // TODO: The RFCPeerServer needs to spawn new threads for each connection
-        // (since many peers should be able to connect at the same time)
-        Connection connection = waitForNewConnection();
-
         while (true) {
-            P2PMessage message;
-            try {
-                message = (P2PMessage) connection.waitForNextCommunication();
-            } catch (RuntimeException e) {
-                if (!(e.getCause() instanceof EOFException)) throw e;
+            final Connection connection = waitForNewConnection();
+            new Thread(() -> {
+                while (true) {
+                    P2PMessage message = (P2PMessage) connection.waitForNextCommunication();
 
-                try {
-                    connection = new Connection(socket.accept());
-                    message = (P2PMessage) connection.waitForNextCommunication();
-                } catch (IOException e1) {
-                    throw new RuntimeException("Unable to set up new connection", e1);
-                }
-            }
+                    if (message == null) break;
 
-            if (message instanceof RFCQueryMessage) {
-                byte[] indexAsArray;
-                synchronized (rfcIndex) {
-                    indexAsArray = Utils.objectToByteArray(rfcIndex);
-                }
-                connection.send(new P2PResponse(Status.SUCCESS, indexAsArray));
-            } else if (message instanceof GetRFCMessage) {
-                byte[] rfcFileAsBytes;
-                try {
-                    FileInputStream fileIn = new FileInputStream(new File(rfcFolder, "rfc" + ((GetRFCMessage) message).getRFCID() + ".txt"));
+                    if (message instanceof RFCQueryMessage) {
+                        byte[] indexAsArray;
+                        synchronized (rfcIndex) {
+                            indexAsArray = Utils.objectToByteArray(rfcIndex);
+                        }
+                        connection.send(new P2PResponse(Status.SUCCESS, indexAsArray));
+                    } else if (message instanceof GetRFCMessage) {
+                        byte[] rfcFileAsBytes;
+                        try {
+                            FileInputStream fileIn = new FileInputStream(new File(rfcFolder, "rfc" + ((GetRFCMessage) message).getRFCID() + ".txt"));
 
-                    rfcFileAsBytes = fileIn.readAllBytes();
-                } catch (FileNotFoundException e) {
-                    throw new RuntimeException("File could not be found!", e);
-                } catch (IOException e) {
-                    throw new RuntimeException("Trouble reading file", e);
+                            rfcFileAsBytes = fileIn.readAllBytes();
+                        } catch (FileNotFoundException e) {
+                            throw new RuntimeException("File could not be found!", e);
+                        } catch (IOException e) {
+                            throw new RuntimeException("Trouble reading file", e);
+                        }
+                        connection.send(new P2PResponse(Status.SUCCESS, rfcFileAsBytes));
+                    } else {
+                        throw new ProtocolException.UnexpectedMessageException(message);
+                    }
                 }
-                connection.send(new P2PResponse(Status.SUCCESS, rfcFileAsBytes));
-            } else {
-                throw new ProtocolException.UnexpectedMessageException(message);
-            }
+            }, "PeerServerConnectionHandler").start();
         }
     }
 
