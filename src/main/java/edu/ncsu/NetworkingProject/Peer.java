56cd54f902223f415d02a76ee36da14c4423c526
Mac Chaffee <macchaffee88@gmail.com>
2019-02-26 16:15:58 -0500

    Prevent different peers form sharing the same rfcIndex

--- src/main/java/edu/ncsu/NetworkingProject/Peer.java
+++ src/main/java/edu/ncsu/NetworkingProject/Peer.java
@@ -1,18 +1,12 @@
 package edu.ncsu.NetworkingProject;
 
-import java.io.File;
-
 /**
  * Runs RFCPeerClient and RFCPeerServer in separate threads
  */
 public class Peer {
 
-    private static RFCIndex rfcIndex = new RFCIndex();
-
-    private static int portNumber;
-    private static String regServerIP;
-
     public static void start(String regServerIP, int portNumber, boolean isTestingScenario) {
+        RFCIndex rfcIndex = new RFCIndex();
         RFCPeerClient client = new RFCPeerClient(regServerIP, portNumber, rfcIndex, isTestingScenario);
         Thread clientThread = new Thread(client);
         clientThread.setName("RFC client thread " + portNumber);
